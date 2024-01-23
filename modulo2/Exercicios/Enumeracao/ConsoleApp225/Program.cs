143674f71a2859e44523b62657ecf163f24d4c0d
Nicole Marie <nicolemarieagnelo@gmail.com>
2024-01-23 19:46:35 -0300

    Adicionando novas informações

---
+++ modulo2/Exercicios/Enumeracao/ConsoleApp225/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ConsoleApp225.Entities;
+using ConsoleApp225.Entities.Enums;
+namespace Console225
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+            Console.WriteLine(txt);
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+        }
+    }
+}
