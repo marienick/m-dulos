9b2747dfd9a6a199653838b15d48c006b2c492b6
Nicole Marie <nicolemarieagnelo@gmail.com>
2024-01-25 10:26:18 -0300

    Adicionando exercícios

---
+++ modulo2/Exercicios/ConsoleApp230/Entities/Post.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp230.Entities
+{
+    class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Post() { }
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+        public void  AddComment(Comment comment)
+        {
+            Comments.Add(comment);
+        }
+        public void ShowComments()
+        {
+            for(int i = 0; i < Comments.Count; i++)
+            {
+                Console.WriteLine(Comments[i]);
+            }
+        }
+        public override string ToString()
+        {
+            return Title + "\n" + Likes + " likes - " + Moment + "\n" + Content + "\nComments:\n";
+        }
+    }
+}
---
+++ modulo2/Exercicios/ConsoleApp230/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using ConsoleApp230.Entities;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Type the data of the post:");
+        Console.WriteLine("Title:");
+        string title = Console.ReadLine();
+        Console.WriteLine("Content:");
+        string content = Console.ReadLine();
+        Console.WriteLine("How many likes is in this post?");
+        int likes = int.Parse(Console.ReadLine());
+        Post post = new Post(DateTime.Now, title, content, likes);
+        Console.Write("Do you want to type any comments?");
+        string answer = Console.ReadLine().ToLower();
+        if (answer == "yes" || answer == "y")
+        {
+            Console.WriteLine("How many?");
+            int numOfComments = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numOfComments; i++)
+            {
+                Console.WriteLine($"Type the {i + 1} comment:");
+                string text = Console.ReadLine();
+                Comment commment = new Comment(text);
+                post.Comments.Add(commment);
+            }
+        }
+        Console.WriteLine(post);
+        post.ShowComments();
+    }
+}
