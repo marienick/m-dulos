65634dbed1c553ebc8ee5608918f70bce4bd8a10
Nicole Marie <nicolemarieagnelo@gmail.com>
2024-02-06 21:41:02 -0300

    Atualizando exercicio

--- modulo2/Exercicios/ConsoleApp232/Program.cs
+++ modulo2/Exercicios/ConsoleApp232/Program.cs
@@ -6,9 +6,42 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            BusinessAccount account = new BusinessAccount(8010,"Bob Browm", 100.0, 500.0);
-            Console.WriteLine(account.Balance);
-            // Porém não é possível mudar o valor dobalance aqui, somente na classe ou na subclasse.
+            using ConsoleApp233.Entities;
+using System;
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc = new Account(1001, "Alex", 0.0);
+            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+            //UPCASTING:
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+            Account acc3 = new SavingsAccount(1004, "Ana", 0.0, 0.1);
+
+            //DOWNCASTING               (casting)
+            BusinessAccount acc4 = (BusinessAccount)acc2;
+            acc4.Loan(100.0);
+           // BusinessAccount acc5 = (BusinessAccount)acc3; (Dá erro)
+           if(acc3 is  BusinessAccount)
+            {
+                BusinessAccount acc5 = (BusinessAccount)acc3; 
+                acc5.Loan(200.1);
+                Console.WriteLine("Loan");
+            }
+           if(acc3 is SavingsAccount)
+            {
+                SavingsAccount acc5 = (SavingsAccount)acc3;
+                acc5.UpdateBalance();
+                Console.WriteLine("Update");
+            }
+
+        }
+    }
+}
         }
     }
 }
