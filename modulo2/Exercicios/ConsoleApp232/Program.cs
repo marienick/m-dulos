0fc7913d61d0b43a2f09dbb5e89d2f948036bdff
Nicole Marie <nicolemarieagnelo@gmail.com>
2024-02-08 20:27:15 -0300

    Atualizando arquivo

--- modulo2/Exercicios/ConsoleApp232/Program.cs
+++ modulo2/Exercicios/ConsoleApp232/Program.cs
@@ -6,19 +6,13 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            using ConsoleApp233.Entities;
-using System;
-namespace Course
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            
+            Account acc1 = new Account(1001, "Alex", 500.0);
+            Account acc2 = new SavingsAccount(1002, "Ana", 500.0,0.01);
+            acc1.Widthdraw(10.0);
+            acc2.Widthdraw(10.0);
+            Console.WriteLine(acc1.Balance);
+            Console.WriteLine(acc2.Balance);
 
-        }
-    }
-}
         }
     }
 }
